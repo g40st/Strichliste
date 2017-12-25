1376d995598a7956f9824bc3e7c1e453d50f054c
Christian Högerle <christian@hoegerle-home.de>
2017-12-25 17:46:25 +0100

    small changes in users

--- src/Singleton.java
+++ src/Singleton.java
@@ -2,26 +2,15 @@ import java.io.*;
 
 public class Singleton {
     private static Singleton singleton;
-    private static final User[] users = new User[16];
+    private static final User[] users = new User[4];
     private static final Price price = new Price(); 
     
     private Singleton() {
-        users[0] = new User("Michi");
-        users[1] = new User("Barth");
-        users[2] = new User("Timo");
-		users[3] = new User("Schwanz");
-        users[4] = new User("Jenny");
-        users[5] = new User("Denise");
-        users[6] = new User("Steck");
-        users[7] = new User("ghost");
-        users[8] = new User("Jan");
-		users[9] = new User("Glaser");
-		users[10] = new User("Karl-Josef");
-		users[11] = new User("Wohnhaas");
-        users[12] = new User("Jannic");
-        users[13] = new User("Pitsch");
-        users[14] = new User("Achim");
-        users[15] = new User("Ralle");
+        users[0] = new User("user1");
+        users[1] = new User("user2");
+        users[2] = new User("user3");
+		users[3] = new User("user4");
+        
     }
 
     public static synchronized Singleton getInstance() {
@@ -98,7 +87,8 @@ class Serial {
     //private final static String fileName = new String("StrichlisteDB.txt");
     
     // Backup auf USB-Stick gemounted unter F: (fuer Windows)
-    private final static String stickFilePath = "F:\\" +fileName; 
+    private final static String stickFilePath = null;
+    //private final static String stickFilePath = "F:\\" +fileName; 
 
     public static void writeToFile(User[] users) {
         try {
@@ -112,14 +102,16 @@ class Serial {
         }
     }
     public static void writeToStick(User[] users) {
-        try {
-            ObjectOutputStream out2 = new ObjectOutputStream(new FileOutputStream(stickFilePath));
-            for (int i = 0; i < users.length ; i++) {
-                out2.writeObject(users[i]);
+        if(stickFilePath != null) {
+            try {
+                ObjectOutputStream out2 = new ObjectOutputStream(new FileOutputStream(stickFilePath));
+                for (int i = 0; i < users.length ; i++) {
+                    out2.writeObject(users[i]);
+                }
+                out2.close();
+            } catch(Exception ex) {
+                System.out.println("Exception " + ex);
             }
-            out2.close();
-        } catch(Exception ex) {
-            System.out.println("Exception " + ex);
         }
     }
     public static void readFromFile(User[] users) {
